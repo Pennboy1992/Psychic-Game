4fd72461fb44cd58ec51723d10ee65c953bff4a1
Mad Marine <pnnroman92@gmail.com>
2019-05-30 15:28:41 -0500

    added javascript code to run random letters, wins, losses, and number of guesses

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guessChoices = [];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+
+    if (options.indexOf(userGuess) > -1) {
+
+        if (userGuess === computerGuess) {
+            wins++;
+            numGuesses = 9;
+            guessChoices = [];
+            document.querySelector("#winner").textContent = wins;
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            console.log(computerGuess);
+        }
+
+        if (userGuess != computerGuess) {
+            numGuesses--;
+            document.querySelector("#guest").textContent = numGuesses;
+            guessChoices.push(userGuess);
+            
+        }
+
+        if (numGuesses === 0) {
+
+            numGuesses = 9;
+            losses++;
+            guessChoices = [];
+            document.querySelector("#loser").textContent = losses;
+            document.querySelector("#cguesses").textContent = guessChoices;
+        
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            console.log(computerGuess);
+        }
+
+  
+
+    }
+};
